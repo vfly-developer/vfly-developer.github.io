1526e798a9d9c0445621e2a0d83d3f6a8f7447fd
Vinh Nguyen <43118720+vfly-developer@users.noreply.github.com>
2020-07-10 17:38:56 -0400

    Updates

---
+++ precache-manifest.704730ff44bb45b1e9eb4507f7f17b14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689816f7c78c7043e3f736f1e3dc87cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b87fe3c0499519c6db0",
+    "url": "/static/css/main.3bce1034.chunk.css"
+  },
+  {
+    "revision": "fd05342cf586c5ea84a5",
+    "url": "/static/js/2.01f27ba4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.01f27ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b87fe3c0499519c6db0",
+    "url": "/static/js/main.a0309d11.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  },
+  {
+    "revision": "06810f08722ff8c331eaced5e1b85441",
+    "url": "/static/media/avatar.06810f08.png"
+  }
+]);
